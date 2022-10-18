e4f92dfedb356eeeb348cfc5d3c1a428d37911e6
GRUPOXCARET\svazquezv <svazquezv@xcaret.com>
2022-10-18 18:40:36 -0500

    quitar url

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config:
 
     #Configuración de base dedatos 
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://turquesa:Pr1vadasTurq3sas@localhost:3306/privadasturquesas"
+    
 
 class ProductionConfig(Config):
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://samuelvazquezver:Pr1vadasTurq3sas@samuelvazquezvera.mysql.pythonanywhere-services.com/samuelvazquezver$privadasturquesas"
+    
     SECRET_KEY = 'configproduccion'
 
 class DevelopmentConfig(Config):
